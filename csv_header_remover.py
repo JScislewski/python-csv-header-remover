f61453022c744c1f303129ae99de4fe17c384eb9
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-11-11 23:49:26 +0100

    Add reading csv files

--- csv_header_remover.py
+++ csv_header_remover.py
@@ -1,7 +1,18 @@
 import os
+import csv
 
 os.makedirs("header_removed", exist_ok=True)
 
 for file in os.listdir("."):
     if file.endswith(".csv"):
         print("Removing header from: " + file + "...")
+
+        csv_rows = []
+        csv_file_obj = open(file)
+        reader_obj = csv.reader(csv_file_obj)
+
+        for row in reader_obj:
+            if reader_obj.line_num != 1:
+                csv_rows.append(row)
+
+        csv_file_obj.close()
