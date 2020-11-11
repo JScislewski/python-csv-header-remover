d46e43e6fb69e09df62cba27e31df705853248b1
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-11-12 00:46:41 +0100

    Refactor to use list comprehension

--- csv_header_remover.py
+++ csv_header_remover.py
@@ -7,13 +7,10 @@ for file in os.listdir("."):
     if file.endswith(".csv"):
         print("Removing header from: " + file + "...")
 
-        csv_rows = []
         csv_file_obj = open(file)
         csv_reader = csv.reader(csv_file_obj)
 
-        for row in csv_reader:
-            if csv_reader.line_num != 1:
-                csv_rows.append(row)
+        csv_rows = [row for row in csv_reader if csv_reader.line_num != 1]
 
         csv_file_obj.close()
         csv_file_obj = open(os.path.join("header_removed", file), "w", newline="")
